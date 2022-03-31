0cc85a006729b57008bbd791043e33064ce4cf76
adrianofinco <adriano.finco.509@gmail.com>
2022-03-30 21:18:02 -0300

    feat: add new components

---
+++ src/components/GenericCard.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="max-w-[350px] mx-auto font-sans normal-case border border-gray-700 rounded-xl tracking-wide px-4 flex items-center bg-black-900 bg-opacity-20 py-8">
+    <p class="">
+      <slot/>
+    </p>
+  </div>
+</template>
