a9b2fc275b83fcb88f5a06140ba39a3af231da19
adrianofinco <adriano.finco.509@gmail.com>
2022-03-31 09:04:26 -0300

    fix: normalize styling of the '404 redirect' page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,27 @@
 <template>
-  <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold leading-tight text-gray-900">Not Found</h1>
-    </div>
-  </header>
+  <div class="py-10 px-6">
+    <GenericCard>
+      <p class="mb-4">
+        The page you were looking for could not be found. Go back to 
+        <a class="underline text-yellow-400 hover:text-yellow-500"
+        href="/time2launch/">main page</a>.
+      </p>
+      <p>
+        If the problem persists
+        <a class="underline text-yellow-400 hover:text-yellow-500" target="_blank" 
+        href="https://github.com/adrianofinco/time2launch/issues">open an issue
+          </a> 
+        on Github.
+      </p>
+    </GenericCard>
+  </div>
 </template>
+
+<script >
+import GenericCard from '@/components/GenericCard.vue';
+
+export default {
+  components: { GenericCard },
+  data () { return {} }
+}
+</script>
